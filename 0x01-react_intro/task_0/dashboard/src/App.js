d19f33369c7ce6aa8c3f0fa0f60f3de4a08bcb90
saria-mohi <saria.m.mohammed@gmail.com>
2023-10-30 10:46:59 +0200

    rededign task0

--- 0x01-react_intro/task_0/dashboard/src/App.js
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -1,21 +1,21 @@
-import logo from './logo.svg';
+import logo from './logo.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-        <div className="App-header">
-        <img src={logo} alt="logo" />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <h1>School dashboard</h1>
-      </div>
-      <div className="App-body">
-        <p>
+
+      </header>
+      
+        <div className="App-body">
           Login to access the full dashboard
-        </p>
-      </div>
-      <div className="App-footer">
+        </div>
+      <footer className="App-footer">
         <p>Copyright 2020 - holberton School</p>
-      </div>
+      </footer>
     </div>
   );
 }
