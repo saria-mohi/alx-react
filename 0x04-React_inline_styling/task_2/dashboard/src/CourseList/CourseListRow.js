7e881f37b93319b8d72043bfa5e32a7f4c2c7829
saria-mohi <saria.m.mohammed@gmail.com>
2023-11-07 20:28:37 +0300

    no repeat

--- 0x04-React_inline_styling/task_2/dashboard/src/CourseList/CourseListRow.js
+++ 0x04-React_inline_styling/task_2/dashboard/src/CourseList/CourseListRow.js
@@ -1,37 +1,40 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { StyleSheet, css } from 'aphrodite';
 
 const CourseListRow = ({ isHeader, textFirstCell, textSecondCell }) => {
-  const bgColor1 = { backgroundColor: '#f5f5f5ab' };
-  const bgColor2 = { backgroundColor: '#deb5b545' };
-  let bgColor = undefined;
-  let content = undefined;
+  const styles = {
+    th: {
+      textAlign: 'left',
+      borderTop: '1px solid gray',
+      borderBottom: '1px solid gray',
+    },
+    evenRow: {
+      backgroundColor: '#f5f5f5ab',
+    },
+    oddRow: {
+      backgroundColor: '#deb5b545',
+    },
+  };
 
-  if (isHeader === true) {
-    bgColor = bgColor2;
-    if (textSecondCell === null) {
-      content = <th colSpan='2'>{textFirstCell}</th>;
-    } else {
-      content = (
-        <Fragment>
-          <th className={css(styles.th)}>{textFirstCell}</th>
-          <th className={css(styles.th)}>{textSecondCell}</th>
-        </Fragment>
-      );
-    }
-  }
-  if (isHeader === false) {
-    bgColor = bgColor1;
-    content = (
-      <Fragment>
-        <td>{textFirstCell}</td>
-        <td>{textSecondCell}</td>
-      </Fragment>
-    );
-  }
+  const rowStyle = isHeader ? {} : isEvenRow ? styles.evenRow : styles.oddRow;
+  const content = isHeader ? (
+    textSecondCell === null ? (
+      <th colSpan='2'>{textFirstCell}</th>
+    ) : (
+      <>
+        <th className={css(styles.th)}>{textFirstCell}</th>
+        <th className={css(styles.th)}>{textSecondCell}</th>
+      </>
+    )
+  ) : (
+    <>
+      <td>{textFirstCell}</td>
+      <td>{textSecondCell}</td>
+    </>
+  );
 
-  return <tr style={bgColor}>{content}</tr>;
+  return <tr style={rowStyle}>{content}</tr>;
 };
 
 CourseListRow.defaultProps = {
@@ -44,22 +47,5 @@ CourseListRow.propTypes = {
   textFirstCell: PropTypes.string.isRequired,
   textSecondCell: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
 };
-
-const styles = StyleSheet.create({
-  row: {
-    backgroundColor: '#deb5b545',
-  },
-  headerBgColor: '#deb5b545',
-  th: {
-    textAlign: 'left',
-    borderTop: '2px solid gray',
-    borderBottom: '2px solid gray',
-    fontWeight: 'bold',
-  },
-  td: {
-    borderTop: '1px solid gray',
-    borderBottom: '1px solid gray',
-  },
-});
 
 export default CourseListRow;
