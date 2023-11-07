252acac9cb383f28758879465e5f5591a1bd5887
saria-mohi <saria.m.mohammed@gmail.com>
2023-11-07 20:34:34 +0300

    no repeat

--- 0x04-React_inline_styling/task_2/dashboard/src/CourseList/CourseListRow.js
+++ 0x04-React_inline_styling/task_2/dashboard/src/CourseList/CourseListRow.js
@@ -1,40 +1,37 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { StyleSheet, css } from 'aphrodite';
 
 const CourseListRow = ({ isHeader, textFirstCell, textSecondCell }) => {
-  const styles = {
-    th: {
-      textAlign: 'left',
-      borderTop: '1px solid gray',
-      borderBottom: '1px solid gray',
-    },
-    evenRow: {
-      backgroundColor: '#f5f5f5ab',
-    },
-    oddRow: {
-      backgroundColor: '#deb5b545',
-    },
-  };
+  const bgColor1 = { backgroundColor: '#f5f5f5ab' };
+  const bgColor2 = { backgroundColor: '#deb5b545' };
+  let bgColor = undefined;
+  let content = undefined;
 
-  const rowStyle = isHeader ? {} : isEvenRow ? styles.evenRow : styles.oddRow;
-  const content = isHeader ? (
-    textSecondCell === null ? (
-      <th colSpan='2'>{textFirstCell}</th>
-    ) : (
-      <>
-        <th className={css(styles.th)}>{textFirstCell}</th>
-        <th className={css(styles.th)}>{textSecondCell}</th>
-      </>
-    )
-  ) : (
-    <>
-      <td>{textFirstCell}</td>
-      <td>{textSecondCell}</td>
-    </>
-  );
+  if (isHeader === true) {
+    bgColor = bgColor2;
+    if (textSecondCell === null) {
+      content = <th colSpan='2'>{textFirstCell}</th>;
+    } else {
+      content = (
+        <Fragment>
+          <th className={css(styles.th)}>{textFirstCell}</th>
+          <th className={css(styles.th)}>{textSecondCell}</th>
+        </Fragment>
+      );
+    }
+  }
+  if (isHeader === false) {
+    bgColor = bgColor1;
+    content = (
+      <Fragment>
+        <td>{textFirstCell}</td>
+        <td>{textSecondCell}</td>
+      </Fragment>
+    );
+  }
 
-  return <tr style={rowStyle}>{content}</tr>;
+  return <tr style={bgColor}>{content}</tr>;
 };
 
 CourseListRow.defaultProps = {
@@ -47,5 +44,8 @@ CourseListRow.propTypes = {
   textFirstCell: PropTypes.string.isRequired,
   textSecondCell: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
 };
+
+
+
 
 export default CourseListRow;
